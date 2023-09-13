fdda78b8aa25156355bbd3116ae07ef7c0d85888
mohamed-eliyas <143862840+mohamed7639@users.noreply.github.com>
2023-09-13 05:15:49 -0700

    i'm submitted source code only

---
+++ index.js
@@ -0,0 +1,271 @@
+//blog section//
+// read more function first-content//
+document.getElementById("span").addEventListener('click',function(){
+  document.getElementById("container").classList.toggle('active');
+})
+
+//read more function second-content//
+document.getElementById("span-two").addEventListener('click',function(){
+  document.getElementById("container-two").classList.toggle('active');
+})
+
+//read more function third-content//
+document.getElementById("span-three").addEventListener('click',function(){
+  document.getElementById("container-three").classList.toggle('active');
+})
+
+//read more function forth-content//
+document.getElementById("span-four").addEventListener('click',function(){
+  document.getElementById("container-four").classList.toggle('active');
+})
+
+//shop section//
+//card popup  function //
+
+function toggle() {
+  var blur=document.getElementById('blur');
+  blur.classList.toggle('active');
+  var popup = document.getElementById('popup');
+  popup.classList.toggle('active');
+}
+//two
+function tog() {
+  var blur=document.getElementById('blur-two');
+  blur.classList.toggle('active-two');
+  var popup = document.getElementById('popup-two');
+  popup.classList.toggle('active-two');
+}
+//three
+function cardshow() {
+  var blur=document.getElementById('blur-three');
+  blur.classList.toggle('active-three');
+  var popup = document.getElementById('popup-three');
+  popup.classList.toggle('active-three');
+}
+//four
+function show() {
+  var blur=document.getElementById('blur-four');
+  blur.classList.toggle('active-four');
+  var popup = document.getElementById('popup-four');
+  popup.classList.toggle('active-four');
+}
+//five
+function showbox() {
+  var blur=document.getElementById('blur-five');
+  blur.classList.toggle('active-five');
+  var popup = document.getElementById('popup-five');
+  popup.classList.toggle('active-five');
+}
+//six
+function box() {
+  var blur=document.getElementById('blur-six');
+  blur.classList.toggle('active-six');
+  var popup = document.getElementById('popup-six');
+  popup.classList.toggle('active-six');
+}
+//seven
+function card() {
+  var blur=document.getElementById('blur-seven');
+  blur.classList.toggle('active-seven');
+  var popup = document.getElementById('popup-seven');
+  popup.classList.toggle('active-seven');
+}
+//eight
+function pop() {
+  var blur=document.getElementById('blur-eight');
+  blur.classList.toggle('active-eight');
+  var popup = document.getElementById('popup-eight');
+  popup.classList.toggle('active-eight');
+}
+
+// printing cotten t-shirt popup card//
+//--------- ------ ------- ----- ----//
+
+function tshirt() {
+  var blur=document.getElementById('blur-a');
+  blur.classList.toggle('active-a');
+  var popup = document.getElementById('popup-a');
+  popup.classList.toggle('active-a');
+}
+//two
+function dis() {
+  var blur=document.getElementById('blur-b');
+  blur.classList.toggle('active-b');
+  var popup = document.getElementById('popup-b');
+  popup.classList.toggle('active-b');
+}
+//three
+function cas() {
+  var blur=document.getElementById('blur-c');
+  blur.classList.toggle('active-c');
+  var popup = document.getElementById('popup-c');
+  popup.classList.toggle('active-c');
+}
+//four
+function see() {
+  var blur=document.getElementById('blur-d');
+  blur.classList.toggle('active-d');
+  var popup = document.getElementById('popup-d');
+  popup.classList.toggle('active-d');
+}
+//five
+function pin() {
+  var blur=document.getElementById('blur-e');
+  blur.classList.toggle('active-e');
+  var popup = document.getElementById('popup-e');
+  popup.classList.toggle('active-e');
+}
+//six
+function team() {
+  var blur=document.getElementById('blur-f');
+  blur.classList.toggle('active-f');
+  var popup = document.getElementById('popup-f');
+  popup.classList.toggle('active-f');
+}
+//seven
+function details() {
+  var blur=document.getElementById('blur-g');
+  blur.classList.toggle('active-g');
+  var popup = document.getElementById('popup-g');
+  popup.classList.toggle('active-g');
+}
+//eight
+function visible() {
+  var blur=document.getElementById('blur-h');
+  blur.classList.toggle('active-h');
+  var popup = document.getElementById('popup-h');
+  popup.classList.toggle('active-h');
+}
+
+// pant popup card section//
+//------------------------//
+
+function aa() {
+  var blur=document.getElementById('blur-aa');
+  blur.classList.toggle('active-aa');
+  var popup = document.getElementById('popup-aa');
+  popup.classList.toggle('active-aa');
+}
+//two
+function bb() {
+  var blur=document.getElementById('blur-bb');
+  blur.classList.toggle('active-bb');
+  var popup = document.getElementById('popup-bb');
+  popup.classList.toggle('active-bb');
+}
+//three
+function cc() {
+  var blur=document.getElementById('blur-cc');
+  blur.classList.toggle('active-cc');
+  var popup = document.getElementById('popup-cc');
+  popup.classList.toggle('active-cc');
+}
+//four
+function dd() {
+  var blur=document.getElementById('blur-dd');
+  blur.classList.toggle('active-dd');
+  var popup = document.getElementById('popup-dd');
+  popup.classList.toggle('active-dd');
+}
+//five
+function ee() {
+  var blur=document.getElementById('blur-ee');
+  blur.classList.toggle('active-ee');
+  var popup = document.getElementById('popup-ee');
+  popup.classList.toggle('active-ee');
+}
+//six
+function ff() {
+  var blur=document.getElementById('blur-ff');
+  blur.classList.toggle('active-ff');
+  var popup = document.getElementById('popup-ff');
+  popup.classList.toggle('active-ff');
+}
+//seven
+function gg() {
+  var blur=document.getElementById('blur-gg');
+  blur.classList.toggle('active-gg');
+  var popup = document.getElementById('popup-gg');
+  popup.classList.toggle('active-gg');
+}
+//eight
+function hh() {
+  var blur=document.getElementById('blur-hh');
+  blur.classList.toggle('active-hh');
+  var popup = document.getElementById('popup-hh');
+  popup.classList.toggle('active-hh');
+}
+
+//shorts popup card function//
+
+
+function i() {
+  var blur=document.getElementById('blur-aaa');
+  blur.classList.toggle('active-aaa');
+  var popup = document.getElementById('popup-aaa');
+  popup.classList.toggle('active-aaa');
+}
+//two
+function j() {
+  var blur=document.getElementById('blur-bbb');
+  blur.classList.toggle('active-bbb');
+  var popup = document.getElementById('popup-bbb');
+  popup.classList.toggle('active-bbb');
+}
+//three
+function k() {
+  var blur=document.getElementById('blur-ccc');
+  blur.classList.toggle('active-ccc');
+  var popup = document.getElementById('popup-ccc');
+  popup.classList.toggle('active-ccc');
+}
+//four
+function l() {
+  var blur=document.getElementById('blur-ddd');
+  blur.classList.toggle('active-ddd');
+  var popup = document.getElementById('popup-ddd');
+  popup.classList.toggle('active-ddd');
+}
+//five
+function m() {
+  var blur=document.getElementById('blur-eee');
+  blur.classList.toggle('active-ee');
+  var popup = document.getElementById('popup-eee');
+  popup.classList.toggle('active-eee');
+}
+//six
+function n() {
+  var blur=document.getElementById('blur-fff');
+  blur.classList.toggle('active-fff');
+  var popup = document.getElementById('popup-fff');
+  popup.classList.toggle('active-fff');
+}
+//seven
+function o() {
+  var blur=document.getElementById('blur-ggg');
+  blur.classList.toggle('active-ggg');
+  var popup = document.getElementById('popup-ggg');
+  popup.classList.toggle('active-ggg');
+}
+//eight
+function p() {
+  var blur=document.getElementById('blur-hhh');
+  blur.classList.toggle('active-hhh');
+  var popup = document.getElementById('popup-hhh');
+  prompt(" why do you cancel this order, please enter your reason");
+  popup.classList.toggle('active-hhh');
+}
+
+//  home page show alert function //
+
+function show(){
+  alert(" the product has not lanched yet...");
+}
+
+//  shop page show alert function //
+
+function msg(){
+  prompt(" why do you cancel this order, please enter your reason");
+}
+
+
